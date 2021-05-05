2643751be3d728614e1472bb294e0cdf9e5e73ef
quang3103 <trannhutquang.bi@gmail.com>
2021-05-05 09:30:51 +0700

    de2i

--- lab1/DE2i_run.v
+++ lab1/DE2i_run.v
@@ -1,9 +1,15 @@
-module DE2i_run(LED, BUTTON, CLOCK_50);
-	input [1:0] BUTTON;
+module DE2i_run(LEDR, LEDG, KEY, CLOCK_50);
+	input [1:0]KEY;
 	input CLOCK_50;
-	output [15:0]LED;
+	output [15:0]LEDR;
+	output [1:0]LEDG;
 	
+	wire clock1s;
+	wire max, min, isFlick;
 	
+	assign LEDG[1:0] = {2{clock1s}};
 	
+	clock_div clk1(.clock50(CLOCK_50), .clock(clock1s));
+	bound_flasher(.led(LEDR), .flick(~KEY[0]), .clk(clock1s), .max(max), .min(min), .isFlick(isFlick));
 	
 endmodule 
---
+++ lab1/DE2i_run.v.bak
@@ -0,0 +1,9 @@
+module DE2i_run(LED, BUTTON, CLOCK_50);
+	input [1:0] BUTTON;
+	input CLOCK_50;
+	output [15:0]LED;
+	
+	
+	
+	
+endmodule 
